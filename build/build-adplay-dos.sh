e46724dea0a95737c47a315327a86a8cccd6ef93
Aran <aranvink@gmail.com>
2023-08-31 17:20:01 +0200

    Add build scripts for components

--- build/build-adplay-dos.sh
+++ build/build-adplay-dos.sh
@@ -1,25 +1,31 @@
-git clone https://github.com/adplug/adplay-dos.git --branch dos-1.6 --single-branch
+# You can also do a clean build from git instead of code copied into Docker container
+# git clone https://github.com/adplug/adplay-dos.git --branch dos-1.6 --single-branch
 cd adplay-dos
 
 #build window
 cd window
-cp Makefile.gcc Makefile
+# Only needed if cloning from older git repo (<=adplay 1.6)
+# cp Makefile.gcc Makefile
 make CC=gcc prefix=/djgpp
 make install CC=gcc prefix=/djgpp
 
 cd ..
 
-cp Makefile.gcc Makefile
+# Only needed if cloning from older git repo (<=adplay 1.6)
+# cp Makefile.gcc Makefile
 #change Makefile
-prefix = ~/djgpp
-iprefix = $(HOME)/djgpp
-CPPFLAGS += -I$(iprefix)/include/libbinio -I$(iprefix)/include -I$(iprefix)/include/adplug
-CXXFLAGS += -I$(iprefix)/include/libbinio -I$(iprefix)/include -I$(iprefix)/include/adplug
+#prefix = /djgpp
+#iprefix = /djgpp
+#CPPFLAGS += -I/djgpp/include/libbinio -I/djgpp/include -I/djgpp/include/adplug
+#CXXFLAGS += -I/djgpp/include/libbinio -I/djgpp/include -I/djgpp/include/adplug
 
-iprefix = $(HOME)/djgpp
-LDFLAGS += -lstdc++ -lwindow -ladplug -lbinio -lgcctimer -L$(iprefix)/lib
-LDFLAGS = -L/home/aranvink/djgpp/lib -L/home/aranvink/djgpp/lib/gcc/i586-pc-msdosdjgpp/12.2.0
+#iprefix = $(HOME)/djgpp
+#LDFLAGS += -lstdc++ -lwindow -ladplug -lbinio -lgcctimer -L/djgpp/lib
+#LDFLAGS += -L/djgpp/lib -L/djgpp/lib/gcc/i586-pc-msdosdjgpp/12.2.0
 
 #change PATH_MAX=256 to adplay.cpp, filewnd.h, filewnd.cpp
+# add to adplay.cpp:
+# // global variables
+# static int PATH_MAX = 256;
 
 make
--- build/build-libbinio.sh
+++ build/build-libbinio.sh
@@ -3,11 +3,15 @@ git clone https://github.com/adplug/libbinio.git --branch libbinio-1.5 --single-
 cd libbinio
 autoreconf --install
 autoupdate
-libtoolize --force && aclocal && autoconf && automake --add-missing && ./configure --enable-maintainer-mode --host=i386-pc-msdosdjgpp
-make prefix=/djgpp
-mkdir /destlibbinio
-make install DESTDIR=/destlibbinio
-cp -R /destlibbinio/usr/local/include /djgpp
-cp -R /destlibbinio/usr/local/lib /djgpp
-cp -R /destlibbinio/usr/local/share /djgpp
+libtoolize --force && aclocal && autoconf && automake --add-missing && ./configure --enable-maintainer-mode --host=i386-pc-msdosdjgpp --prefix=/djgpp
+make install
+# # /djgpp/lib/libbinio.la needs to be patched, for some reason dependency_libs is not set correctly and is prefixed with /usr/local
+# patch copied libbinio.la in /djgpp/lib to point to correct stdc++ path
+# Not sure why this is needed, could not find a way to correct this from configure/build
+# Changes:
+# dependency_libs=' /usr/local/djgpp/lib/gcc/i586-pc-msdosdjgpp/12.2.0/libstdc++.la'
+# To:
+# dependency_libs=' /djgpp/lib/gcc/i586-pc-msdosdjgpp/12.2.0/libstdc++.la'
+
+cp -v /build/libbinio-patches/libbinio.la /djgpp/lib/
 cd ..
