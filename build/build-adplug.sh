c029582adfd4ecb6f1a1d5e9aaf41f0880fc2f51
Aran <aranvink@gmail.com>
2023-09-02 22:25:31 +0200

    Cleanup build script

--- build/build-adplug.sh
+++ build/build-adplug.sh
@@ -7,9 +7,8 @@ autoreconf
 automake --add-missing
 autoreconf --install
 autoupdate
-#PKG_CONFIG_PATH=/djgpp/lib
 export libbinio_CFLAGS=-I/djgpp/include/libbinio
-export libbinio_LIBS=-L/djgpp/lib #-lbinio
+export libbinio_LIBS=-L/djgpp/lib
 
 libtoolize --force
 aclocal
@@ -17,20 +16,13 @@ autoconf
 automake --add-missing
 ./configure --host=i386-pc-msdosdjgpp --prefix=/djgpp iprefix=/djgpp
 
-# patch makefile
+# patches to makefile
 # CXXFLAGS += -Wno-deprecated -I/djgpp/include/libbinio
 # CPPFLAGS += -Wno-deprecated -I/djgpp/include/libbinio
 # LDFLAGS += -lstdc++ -lbinio -lgcctimer -L/djgpp/lib
 
 cp -v /build/adplay-dos/build/adplug-patches/Makefile /build/adplug/Makefile
 
-# patch getopt.c #include "getopt.h" > #include "mygetopt.h"
-
-# patch player.h string > string.h
-# patch rix.h   unsigned int getsubsongs();
-# patch rix.cpp unsigned int CrixPlayer::getsubsongs()
-
-#cp -Rv /build/adplay-dos/build/adplug-patches/* /build/adplug
 patch /build/adplug/src/adplug.h < /build/adplay-dos/build/adplug-patches/src/adplug.h.patch
 patch /build/adplug/src/player.h < /build/adplay-dos/build/adplug-patches/src/player.h.patch
 patch /build/adplug/src/rix.h < /build/adplay-dos/build/adplug-patches/src/rix.h.patch
